3b23435105b054cb1ee0f0e82c0b5d17f2a064a7
HeJiaomy <121916986@qq.com>
2017-12-07 21:43:07 +0800

    Initial commit

---
+++ app/src/main/java/com/newsclient/News.java
@@ -0,0 +1,26 @@
+package com.newsclient;
+
+/**
+ * Created by hejiao on 2017/12/6.
+ */
+
+class News {
+    public String title;
+    public String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
